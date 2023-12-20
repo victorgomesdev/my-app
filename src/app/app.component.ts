edc4074ef105b33d226abb7f31844b0d3f745f22
Victor Gomes Nogueira <victorgomesnog123@gmail.com>
2023-12-20 20:44:14 -0300

    Iniciando repositório

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, Header],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    standalone: true,
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css',
+    selector: 'app-header'
+})
+export class Header {
+
+    
+}
